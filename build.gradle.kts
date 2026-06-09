b0c739d534f101f705dab848bcef3f3dbdb706f0
byimam2nd <byimam2nd@gmail.com>
2026-06-09 15:35:45 +0000

    fix: gunakan submodule artifact com.github.recloudstream.gradle:gradle (bukan root wrapper kosong)

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:8.13.2")
-        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
+        classpath("com.github.recloudstream.gradle:gradle:master-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
         
         // Code quality plugins
